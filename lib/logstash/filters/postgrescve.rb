5cafa7e792c600747833fb0852cb83d7381f659f
ljblancoredborder <ljblanco@redborder.com>
2026-02-19 12:33:15 +0000

    clean logger and tenting helper

--- lib/logstash/filters/postgrescve.rb
+++ lib/logstash/filters/postgrescve.rb
@@ -28,15 +28,13 @@ module LogStash
       end
 
       def filter(event)
-        @logger.info '[PostgresCVE] filter: Input received.'
+        @logger.info '[PostgresCVE][filter]: Input received.'
 
         input_event = event.to_hash
         input_event.delete('@timestamp')
         input_event.delete('@version')
 
-        @logger.info("[PostgresCVE]: Clean input: #{input_event}")
         cpe = input_event['cpe']
-        @logger.info "[PostgresCVE]: New cpe to be fetched: #{cpe}"
         cpe_p_v = get_prod_version(cpe)
         return event.cancel if cpe_p_v.empty?
 
@@ -49,22 +47,16 @@ module LogStash
           without_versions = cpe_p_v[1].nil?
 
           cve_list = []
-          # @logger.info("[PostgresCVE][Filter]: DB response: #{db_response}")
           db_response.each do |row|
             document = JSON.parse(row['data'])
-            @logger.info("[PostgresCVE][Filter]: To find: #{cpe_p_v}")
-            # @logger.info("[PostgresCVE][Filter]: In document: #{document}")
-            # @logger.info("[PostgresCVE][Filter]: Without versions: #{without_versions}")
             cve_list += find_cpe(cpe_p_v, document, without_versions)
           end
 
-          @logger.info("[PostgresCVE][Filter]: Cve list: #{cve_list}")
           cve_list.each do |cve|
             output_event = set_output_event(input_event, cve)
             yield output_event
             @cpes_availables[cpe].push(cve)
           end
-          @logger.info("[PostgresCVE]: New cpe to be fetched: #{cpe}")
           @cpes_availables[cpe].each do |saved_cve|
             output_event = set_output_event(input_event, saved_cve)
             yield output_event
@@ -74,11 +66,6 @@ module LogStash
       end
 
       def pg_conn_info
-        # @database = 'redborder'
-        # @host = 'master.postgresql.service'
-        # @password = 'n2w7o8GUdctxl2zWxkLLj6ryuO11Z433sNP8w294Ay7G7DQMybfaK8hXQ7v2lczcYQ8kbCRSZO5dsoV68XvTqsB5XqaPvGUELqxtB0KQpED29GLgAfERSPJ1mq7YCt68'
-        # @port = 5432
-        # @user = 'redborder'
         {
           dbname: @database,
           user: @user,
@@ -89,14 +76,11 @@ module LogStash
       end
 
       def set_output_event(input_event, cve_info)
-        @logger.info '[PostgresCVE][set_output_event]: Gathering output data'
         cve_info = {} unless cve_info.is_a?(Hash)
         output_hash = input_event.merge(cve_info)
 
-        @logger.info '[PostgresCVE][set_output_event]: Building output event'
         out_event = LogStash::Event.new
         output_hash.each do |k, v|
-          @logger.info "[PostgresCVE][set_output_event]: Set key=#{k}, value=#{v.inspect} in new event"
           out_event.set(k.to_s, v)
         rescue StandardError => e
           @logger.warn "[PostgresCVE][set_output_event]: Failed to set key=#{k}, value=#{v.inspect} - #{e.message}"
@@ -109,14 +93,11 @@ module LogStash
       def get_prod_version(cpe_orig)
         return [] unless cpe_orig
 
-        @logger.info '[PostgresCVE][get_prod_version]: Starting'
         result = []
         cpe_vendor_product_version = cpe_orig.match('cpe:2.3:a:') ? cpe_orig.split('cpe:2.3:a:')[-1] : cpe_orig
         parts = cpe_vendor_product_version.split(':')
         result.push(parts[0..1].join(':'))
         result.push(parts[2]) if parts.length >= 3
-
-        @logger.info("[PostgresCVE][get_prod_version]: Ending with #{result}")
         result
       end
 
@@ -134,12 +115,7 @@ module LogStash
       def find_cpe(cpe, document, without_versions)
         # input:  string cpe as key and hash document as complex hash map.
         # output: array
-        @logger.info '[PostgresCVE][Find cpe]: Starting'
         cves = [] # will be filled with hashes with cve info
-        # @logger.debug("Document: #{document}")
-        # @logger.debug("Document CVE: #{document['cve']}")
-        # @logger.info("[PostgresCVE][Find cpe]: configurations: #{document['cve']['configurations']}")
-        # @logger.debug("Document NODES FIRST: #{document['cve']['configurations'].first['nodes']}")
         configurations = document.dig('cve', 'configurations') || [] # CHECKED
         nodes = configurations.flat_map { |h| h['nodes'] || [] } || []
         @logger.error '[PostgresCVE][filter]: Nodes not present in cves' if nodes.empty?
@@ -154,9 +130,7 @@ module LogStash
       end
 
       def scroll_cpe_match(cpe, cpe_match, without_versions)
-        @logger.info('[PostgresCVE]: Starting [DEBUG][scroll_cpe_match]')
         matched = cpe_match.any? do |elem|
-          @logger.info("[PostgresCVE]: Starting [DEBUG][scroll_cpe_match]: criteria: #{elem['criteria']}")
           cpe_db = get_prod_version(elem['criteria'])
           if cpe[0] == cpe_db[0]
             if without_versions
@@ -172,12 +146,10 @@ module LogStash
             false
           end
         end
-        @logger.info('[PostgresCVE]: Ending [DEBUG][scroll_cpe_match]')
         matched
       end
 
       def version_range(cpe, cpe_match_elem)
-        @logger.info('[PostgresCVE]: Starting [DEBUG][version_range]')
         inside_range = false
         if cpe_match_elem.key?('versionEndExcluding')
           inside_range = if cpe_match_elem.key?('versionStartIncluding')
@@ -213,7 +185,6 @@ module LogStash
            !cpe_match_elem.key?('versionEndExcluding') && !cpe_match_elem.key?('versionEndIncluding')
           inside_range = true
         end
-        @logger.info('[PostgresCVE]: Ending [DEBUG][version_range]')
         inside_range
       end
 
@@ -231,9 +202,6 @@ module LogStash
       end
 
       def get_cve_data(document)
-        @logger.info '[PostgresCVE]: Starting [DEBUG][get_cve_data]'
-        @logger.info "[PostgresCVE][get_cve_data]: Document: #{document}"
-
         cve_extra = {
           id: nil,
           cve_info: nil,
@@ -261,7 +229,6 @@ module LogStash
           end
 
           if cvss3_key
-            @logger.info '[PostgresCVE][get_cve_data]: Version 3'
             cve_extra[:metric] = 'cvssV3'
 
             cvss3_data = metrics[cvss3_key]
@@ -279,7 +246,6 @@ module LogStash
             end
 
           elsif metrics['cvssMetricV2']
-            @logger.info '[PostgresCVE][get_cve_data]: Version 2'
             cve_extra[:metric] = 'cvssV2'
 
             cvss2_data = metrics['cvssMetricV2']
@@ -308,17 +274,11 @@ module LogStash
       end
 
       def database(cpe_vendor_product)
-        @logger.info("[PostgresCVE][database] Quering: #{cpe_vendor_product}")
         sql = <<~SQL
           SELECT data FROM cves
           WHERE data::text ILIKE '%:a:#{cpe_vendor_product}:%'
           LIMIT 1;
         SQL
-
-        # for example             select * from cves where data::text ilike '.*:a:nginx:nginx:.*' limit 1;
-        # for example             select * from cves where data::text ilike '%cpe:2.3:a:eric_allman:sendmail:5.58%' limit 1;
-        # metaesploitable example select * from cves where data::text ilike '%:a:mysql:mysql%' limit 1;
-        # metaesploitable example select data from cves where data::text ilike '%mysql:mysql%' limit 1;
 
         result = @conn.exec(sql)
         result.to_a
@@ -329,97 +289,3 @@ module LogStash
     end
   end
 end
-
-## Helpers for directly debugging in IRB:
-
-# def debug
-#   @cpes_availables = nil
-#   event = {
-#     "cpe": "cpe:2.3:a:unrealircd:unrealircd",
-#     "scan_id": "9",
-#     "scan_type": "2",
-#     "name": "scanner",
-#     "uuid": "8534becf-c628-4a9a-8e96-f5b75afcaf7e",
-#     "campus": "",
-#     "campus_uuid": "",
-#     "deployment": "",
-#     "deployment_uuid": "",
-#     "zone": "",
-#     "zone_uuid": "",
-#     "market": "",
-#     "market_uuid": "",
-#     "floor": "",
-#     "floor_uuid": "",
-#     "ipv4": "10.1.32.81",
-#     "timestamp": 1771410211,
-#     "product": "UnrealIRCd",
-#     "version": nil,
-#     "servicename": nil,
-#     "protocol": "tcp",
-#     "port_state": "open",
-#     "port": "6667"
-#   }
-#   filter event
-# end
-
-# class Hash
-#   def cancel
-#    puts "SIMULACRO: EVENT CANCELLED: "
-#    self
-#    puts "END SIMULACRO: EVENT CANCELLED: "
-#   end
-# end
-
-# Simple Event class to simulate Logstash::Event
-# class EventDebug
-#   attr_accessor :fields, :metadata
-
-#   def initialize(initial_fields = {})
-#     @fields = initial_fields.dup
-#     @metadata = {
-#       "@timestamp" => Time.now.utc,
-#       "@version" => "1"
-#     }
-#   end
-
-#   def remove(key)
-#     @fields.delete(key)
-#   end
-
-#   # Hash-like access
-#   def [](key)
-#     @fields[key]
-#   end
-
-#   def []=(key, value)
-#     @fields[key] = value
-#   end
-
-#   # Logstash-style set/get methods
-#   def set(key, value)
-#     @fields[key] = value
-#   end
-
-#   def get(key)
-#     @fields[key]
-#   end
-
-#   # Tag support
-#   def tag(tag_name)
-#     @fields["tags"] ||= []
-#     @fields["tags"] << tag_name
-#   end
-
-#   # Return combined hash
-#   def to_h
-#     @metadata.merge(@fields)
-#   end
-# end
-
-# Usage
-# event = EventDebug.new
-# event.set("message", "Hello CVE")
-# event.set("severity", "high")
-# event.tag("cve_import")
-
-# puts event.to_h
